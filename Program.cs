f3144dda0e0691273dd39517df9b2fbf33a085e1
michaldrozd <michaldrozd03@gmail.com>
2024-11-27 17:21:25 +0100

    lab2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
-﻿Lab1.Zad1();
-Lab1.Zad2();
-Lab1.Zad3();
-Lab1.Zad4Silnia(4);
-Lab1.Zad5();
+﻿// Lab1.Zad1();
+// Lab1.Zad2();
+// Lab1.Zad3();
+// Lab1.Zad4Silnia(4);
+// Lab1.Zad5();
+
+using Programowanie_Objektowe_w69971_SL02;
+
+var sumator = new Sumator([1,2,3,4,5]);
+sumator.Indexer(2, 4);
---
+++ Sumator.cs
@@ -0,0 +1,39 @@
+﻿namespace Programowanie_Objektowe_w69971_SL02;
+
+public class Sumator
+{
+    private List<int> liczby;
+
+    public Sumator(List<int> liczby)
+    {
+        this.liczby = liczby;
+    }
+
+    public int Suma()
+    {
+        return liczby.Sum();
+    }
+
+    public float Podzile3()
+    {
+        return liczby.Sum() / 3;
+    }
+
+    public int IleElementow()
+    {
+        return liczby.Count;
+    }
+
+    public void Wypisz()
+    {
+        liczby.ForEach(Console.WriteLine);
+    }
+
+    public void Indexer(int lowIndex, int highIndex)
+    {
+        foreach (var liczba in liczby.Where(x => x >= lowIndex && x <= highIndex))
+        {
+            Console.WriteLine(liczba);
+        }
+    }
+}
